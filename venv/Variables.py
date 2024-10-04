10cb217920f822ba5b5bcf8ac2e85cffec8f337d
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-10-04 21:18:48 +0600

    first commit

---
+++ venv/Variables.py
@@ -0,0 +1,9 @@
+name = "Akif"
+type_of_car = "Mercedes"
+age = "25"
+
+print(name+type_of_car)
+print(name+" "+type_of_car)
+print("My name is "+name)
+print("I'm ",age,"yesrs old")
+print("i am "+age+"years old")
